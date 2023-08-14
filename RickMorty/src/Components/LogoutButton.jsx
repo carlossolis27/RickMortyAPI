a444c2af5a23406e46f56a2b218234dc7306c076
Maikkel43 <127003959+Maikkel43@users.noreply.github.com>
2023-08-14 13:50:18 -0600

    responsividad
    los ultimos ajustes para la responsividad sobre todo en tablets, agregacion de mediaqueries

--- RickMorty/src/Components/LogoutButton.jsx
+++ RickMorty/src/Components/LogoutButton.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { useAuth0 } from '@auth0/auth0-react'
+import'../Pages/welcome.css'
 
 export default function LogoutButton() {
   const { logout } = useAuth0()
   return (
     <div>
-      <button onClick={() => logout()}>Logout</button>
+      <button className='logoutButton' onClick={() => logout()}>Logout</button>
     </div>
   )
 }
