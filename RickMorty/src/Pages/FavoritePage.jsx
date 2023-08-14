4b10cc8f330a59346e8f0b2878c84cd8367f7f52
Ramon Solis Nuñez <ramonsolis11@gmail.com>
2023-08-13 23:12:33 -0600

    ./git.sh

--- RickMorty/src/Pages/FavoritePage.jsx
+++ RickMorty/src/Pages/FavoritePage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useFavoriteContext } from '../Context/FavoriteContext';
+import { useFavoriteContext } from '../../Context/FavoriteContext';
 
 const FavoritesPage = () => {
     const { favoriteCharacters, removeFavoriteCharacter } = useFavoriteContext();
