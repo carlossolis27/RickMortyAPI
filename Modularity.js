7acef0290eed3201e12b0fbd91ea654097d0586a
Ramón Solís Núñez <ramonsolis11@gmail.com>
2024-01-08 15:36:25 -0600

    Create Modularity.js

---
+++ Modularity.js
@@ -0,0 +1,26 @@
+async function fetchData(url) {
+  try {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+// Usage
+const apiUrl = 'https://rickandmortyapi.com/api/character';
+fetchData(apiUrl)
+  .then(data => {
+    // Process the data
+    console.log(data);
+  })
+  .catch(error => {
+    console.error('There was a problem with the fetch operation:', error);
+  });
+
